12ab3f54edb2f267e71779ce0aff214f93a55f51
jagh <jagh1729@gmail.com>
2021-07-14 10:21:51 +0000

    Updated gpu selection for training on Nerve machine

--- nnunet/network_architecture/initialization.py
+++ nnunet/network_architecture/initialization.py
@@ -12,8 +12,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import os
-os.environ['CUDA_VISIBLE_DEVICES']='4'
+#import os
+#os.environ['CUDA_VISIBLE_DEVICES']='4'
 
 from torch import nn
 
