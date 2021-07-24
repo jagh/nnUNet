06cb9b976994d2374d18781a7ceb17400c8bd7c8
John Anderson Garcia <jagh1729@gmail.com>
2021-07-24 14:48:40 +0200

    Updated labels for segmentation

--- data2json.py
+++ data2json.py
@@ -6,7 +6,10 @@ from nnunet.dataset_conversion import utils
 
 
 
-dataset_folder = '/data/01_UB/nnUNet_Sandbox/nnUNet_raw_data_base/nnUNet_raw_data/Task115_COVIDSegChallenge'
+#dataset_folder = '/data/01_UB/nnUNet_Sandbox/nnUNet_raw_data_base/nnUNet_raw_data/Task115_COVIDSegChallenge'
+
+dataset_folder = '/home/jgarcia/datasets/nnUNet_Sandbox/nnUNet_raw_data_base/nnUNet_raw_data/Task115_FullCOVIDSegChallenge'
+
 
 imagesTr_path = os.path.join(dataset_folder, "imagesTr")
 labelsTr_path = os.path.join(dataset_folder, "labelsTr")
@@ -25,10 +28,11 @@ output_file             = os.path.join(dataset_folder, "dataset.json")
 imagesTr_dir            = os.path.join(dataset_folder, "imagesTr")
 imagesTs_dir            = os.path.join(dataset_folder, "imagesTs")
 modalities              = ["CT"]
-labels                  = {0: 'background', 1: 'lesion'}
+labels                  = {0: 'background', 1: 'GGO', 2: 'CON', 3: 'ATE',
+                                            4: 'PLE', 5: 'BAN', 6: 'TBR'}
 dataset_name            = "Task115_COVID-19",
 license                 = "Hands on",
-dataset_description     = "Lesion segmentation for covid+",
+dataset_description     = "General Lesion segmentation for covid+",
 dataset_reference       = "COVID-19-20 - Grand Challenge & MICCAI",
 dataset_release         = '0.0'
 
@@ -39,4 +43,4 @@ utils.generate_dataset_json(output_file, imagesTr_dir, imagesTs_dir, modalities,
 
 ## nnUNET Laucher
 ## Step-1: nnUNet_plan_and_preprocess -t 115 --verify_dataset_integrity
-## Step-2: nnUNet_train 3d_fullres nnUNetTrainerV2 115 0
+## Step-2: CUDA_VISIBLE_DEVICES=4 nnUNet_train 3d_fullres nnUNetTrainerV2 115 0
